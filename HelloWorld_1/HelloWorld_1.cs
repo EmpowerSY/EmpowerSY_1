ce17427773ee39826879d4774bbf05dea15209a4
suntzu72 <sunyou72@hotmail.com>
2023-12-05 14:21:27 +0100

    comment

--- HelloWorld_1/HelloWorld_1.cs
+++ HelloWorld_1/HelloWorld_1.cs
@@ -17,6 +17,7 @@ namespace HelloWorld_1
 		/// <param name="engine">Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
+			// this generate some information
 			engine.GenerateInformation("Hello World");
 		}
 	}
